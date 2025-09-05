6ba9d3f1c8a4f890ccbfb80f030688f45881b3ee
lewan <quangle1113@gmail.com>
2025-09-05 12:32:36 +0200

    Add database

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./db');
 const app = express();
 const port = 3000;
+
+connectDB();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
